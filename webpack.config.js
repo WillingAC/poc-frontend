de803a98affa920b963224168ca14e3d6a9e4050
chenyiquan <yiquan.chen@qingclass.com>
2018-12-19 11:36:47 +0800

    Progress: webpack guide, before HMR

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // generate target index.html
+const CleanWebpackPlugin = require('clean-webpack-plugin'); // Clean up output directory
+const webpack = require('webpack');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        app: './src/index.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+        hot: true,
+        port: 9060,
+        open: false,
+        openPage: '/index.html'
+    },
+    output: {
+        filename: '[name].bundle.js', // output main js filename
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
+    }
+};
